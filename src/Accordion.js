414694bfc5cbe2277b6737c145f12ac8295c2670
Jacob Sherman <jacob@jbsherman.com>
2018-11-06 20:06:50 -0500

    fix: import propTypes and creat-react-class fix on accordion.js

--- src/Accordion.js
+++ src/Accordion.js
@@ -1,68 +1,58 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Collapsible from 'react-collapsible';
+import PropTypes from 'prop-types';
+var createReactClass = require('create-react-class');
 
-var Accordion = React.createClass({
+var Accordion = createReactClass({
 
-  //Set validation for prop types
-  propTypes: {
-    transitionTime: PropTypes.number,
-    easing: PropTypes.string,
-    startPosition: PropTypes.number,
-    classParentString: PropTypes.string,
-    onTrigerClick: PropTypes.func,
-    closeable: PropTypes.bool,
-    children: PropTypes.arrayOf(PropTypes.shape({
-      props: PropTypes.shape({
-        'data-trigger': PropTypes.oneOfType([
-          PropTypes.string,
-          PropTypes.element
-        ]).isRequired,
-        'data-triggerWhenOpen': PropTypes.oneOfType([
-          PropTypes.string,
-          PropTypes.element
-        ]),        
-        'data-triggerDisabled': PropTypes.bool,
-      })
-    }))
-  },
+    //Set validation for prop types
+    propTypes: {
+        transitionTime: PropTypes.number,
+        easing: PropTypes.string,
+        startPosition: PropTypes.number,
+        classParentString: PropTypes.string,
+        children: PropTypes.arrayOf(PropTypes.shape({
+            props: PropTypes.shape({
+                'data-trigger': PropTypes.oneOfType([
+                    PropTypes.string,
+                    PropTypes.element
+                ]).isRequired,
+                'data-triggerWhenOpen': PropTypes.oneOfType([
+                    PropTypes.string,
+                    PropTypes.element
+                ]),
+                'data-triggerDisabled': PropTypes.bool,
+            })
+        }))
+    },
 
-  getInitialState: function() {
+    getInitialState: function() {
 
-    //Allow the start position to be set by props
-    var openPosition = this.props.startPosition | 0;
+        //Allow the start position to be set by props
+        var openPosition = this.props.startPosition | 0;
 
-    return {
-      openPosition: openPosition
-    }
-  },
+        return {
+            openPosition: openPosition
+        }
+    },
 
-  handleTriggerClick: function(position) {
-    let closeAll = false;
+    handleTriggerClick: function(position) {
+        this.setState({
+            openPosition: position
+        });
+    },
 
-    if (this.props.closeable) {
-      closeAll = (!this.state.closeAll && position === this.state.openPosition);
-    }
+    render: function() {
 
-    this.setState({
-      openPosition: position,
-      closeAll: closeAll,
-    });
-    
-    this.props.onTriggerClick(position);
-  },
+        var nodes = this.props.children.map((node, index) => {
 
-  render: function() {
+            var triggerWhenOpen = (node.props['data-trigger-when-open']) ? node.props['data-trigger-when-open'] : node.props['data-trigger'];
+            var triggerDisabled = (node.props['data-trigger-disabled']) || false;
 
-    var nodes = this.props.children.map((node, index) => {
-
-      var triggerWhenOpen = (node.props['data-trigger-when-open']) ? node.props['data-trigger-when-open'] : node.props['data-trigger'];
-      var triggerDisabled = (node.props['data-trigger-disabled']) || false;
-
-      return (<Collapsible
+            return (<Collapsible
                 key={"Collapsible"+index}
                 handleTriggerClick={this.handleTriggerClick}
-                open={(!this.state.closeAll && this.state.openPosition === index)}
+                open={(this.state.openPosition === index)}
                 trigger={node.props['data-trigger']}
                 triggerWhenOpen={triggerWhenOpen}
                 triggerDisabled={triggerDisabled}
@@ -70,10 +60,10 @@ var Accordion = React.createClass({
                 easing={this.props.easing}
                 classParentString={this.props.classParentString}
                 accordionPosition={index}>{node}</Collapsible>);
-    });
+        });
 
-    return (<div>{nodes}</div>);
-  }
+        return (<div>{nodes}</div>);
+    }
 
 });
 
